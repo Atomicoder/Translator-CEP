fb497f644f5072e84741128d2954d0641289b467
Sergio Boldu <s.boldu@ftv.local>
2020-03-13 10:16:22 +0100

    types

---
+++ jsx/smallTest.jsx
@@ -0,0 +1,21 @@
+var seq = app.project.activeSequence;
+var vTracks = seq.videoTracks;
+var oneTrack = vTracks[1];
+var trkClips = oneTrack.clips;
+
+var mog = trkClips[0];
+var prps = mog.properties
+
+
+    $.writeln(prps[0])
+  
+
+
+
+
+
+
+
+
+
+
---
+++ text.js
@@ -0,0 +1,28 @@
+{
+    "capPropFontEdit": false,
+    "capPropFontFauxStyleEdit": false,
+    "capPropFontSizeEdit": false,
+    "capPropTextRunCount": 1,
+    "fontEditValue": [
+        "ProximaNova-Black"
+    ],
+    "fontFSAllCapsValue": [
+        true
+    ],
+    "fontFSBoldValue": [
+        false
+    ],
+    "fontFSItalicValue": [
+        false
+    ],
+    "fontFSSmallCapsValue": [
+        false
+    ],
+    "fontSizeEditValue": [
+        110
+    ],
+    "fontTextRunLength": [
+        5
+    ],
+    "textEditValue": "holaf"
+}
